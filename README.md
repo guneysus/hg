a3d0feb57cd97e38ac0ced5aac6832d60d0dc49d
Ahmed Seref Guneysu <ahmed.guneysu@invicti.com>
2024-08-15 23:04:59 +0300

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# __template__
+# HG: DSL for Html Generation
+
+
+HG, abuses operator overloads in C# to implement DSL (Domain Specific Language) for HTML generation.
+
+
+It is very early stage. It does not support 
+
+- Self closing tags
+- Attributes
+
+yet.
+
+## Usage
+
+```csharp
+using static Hg.HgExtensions;
+
+var cursor = ul >
+				li / "Item #1"
+				+ li / "Item #2"
+				+ li / "Item #3";
+
+var html = cursor.Html();
+
+```
---
+++ src/Hg.Tests/VisitorTests.cs
@@ -0,0 +1,51 @@
+﻿using Shouldly;
+using System.Text;
+using Xunit.Abstractions;
+using static Hg.HgExtensions;
+
+namespace Hg.Tests;
+
+public class VisitorTests
+{
+    private readonly ITestOutputHelper Out;
+
+    public VisitorTests(ITestOutputHelper @out)
+    {
+        this.Out = @out;
+    }
+
+    [Fact]
+    public void Simple_Debug()
+    {
+        var cursor = ul >
+                li / "Item #1"
+                + li / "Item #2"
+                + li / "Item #3";
+
+        var sb = new StringBuilder();
+        cursor.VisitTree(onEnter: node =>
+        {
+            sb.Append($"<{node.TagName}>");
+        }, onExit: node =>
+        {
+            if (!string.IsNullOrEmpty(node.Text))
+                sb.Append(node.Text);
+            sb.Append($"</{node.TagName}>");
+        });
+
+        string html = sb.ToString();
+        Out.WriteLine(html);
+        html.ShouldBe("<ul><li>Item #1</li><li>Item #2</li><li>Item #3</li></ul>");
+    }
+
+    [Fact]
+    public void Get_Html_Output()
+    {
+        var cursor = ul >
+                li / "Item #1"
+                + li / "Item #2"
+                + li / "Item #3";
+        string html = cursor.Html();
+        html.ShouldBe("<ul><li>Item #1</li><li>Item #2</li><li>Item #3</li></ul>");
+    }
+}
